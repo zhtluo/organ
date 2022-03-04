396714f5e99ee96dd35de4adfa8dce6a2ed0ae41
zhtluo <zhtluo@gmail.com>
2022-03-04 13:29:53 -0500

    update test script

--- script/test.sh
+++ script/test.sh
@@ -1,11 +1,11 @@
 set -e
 mkdir -p ./log/$2
-RUST_LOG=debug ./target/release/organ config $2 ./log/$2/
-RUST_LOG=debug ./target/release/organ server 0 $2 ./log/$2/bits_32_relay.txt ./log/$2/bits_226_relay.txt  2> >(tee -a ./log/$2/relay.log >&2) &
+RUST_LOG=info ./target/release/organ config $2 ./log/$2/
+RUST_LOG=info ./target/release/organ server 0 $2 ./log/$2/bits_32_relay.txt ./log/$2/bits_226_relay.txt  2> >(tee -a ./log/$2/relay.log >&2) &
 sleep 1
 for ((i = 0; i < $1; i++))
 do
-	RUST_LOG=debug ./target/release/organ client $i $2 ./log/$2/bits_32_nid_$i.txt ./log/$2/bits_226_nid_$i.txt 2> ./log/$2/client_$i.log &
+	RUST_LOG=info ./target/release/organ client $i $2 ./log/$2/bits_32_nid_$i.txt ./log/$2/bits_226_nid_$i.txt 2> ./log/$2/client_$i.log &
 done
 
 wait
