4e6a4bc455a1beb329415069aeb8a9a3e4b6e345
zhtluo <zhtluo@gmail.com>
2022-03-06 17:36:11 -0500

    updated config

--- script/run_client.sh
+++ script/run_client.sh
@@ -1,4 +1,4 @@
-script="sleep 10; rm -f ./output_$2.log; RUST_LOG=info ./organ/target/release/organ client $4 ./organ/script/config/$2/$3.json ./organ/log/$2/bits_32_relay.txt ./organ/log/$2/bits_226_relay.txt 2> output_$2.log"
+script="sleep 10; rm -f ./output_$2.log; RUST_LOG=info ./organ/target/release/organ client $4 ./organ/script/config/$3 ./organ/log/$2/bits_32_relay.txt ./organ/log/$2/bits_226_relay.txt 2> output_$2.log"
 ssh -i ~/organ.pem ubuntu@$1 $script
-mkdir -p ../../log/$2/$3/
-scp -i ~/organ.pem ubuntu@$1:./output_$2.log ../../log/$2/$3/client_$4.log
+mkdir -p ../../log/$3/
+scp -i ~/organ.pem ubuntu@$1:./output_$2.log ../../log/$3/client_$4.log
--- script/run_server.sh
+++ script/run_server.sh
@@ -1,4 +1,4 @@
-script="rm -f ./output.log; RUST_LOG=info ./organ/target/release/organ server 0 ./organ/script/config/$2/$3.json ./organ/log/$2/bits_32_relay.txt ./organ/log/$2/bits_226_relay.txt 2> >(tee -a output.log >&2)"
+script="rm -f ./output.log; RUST_LOG=info ./organ/target/release/organ server 0 ./organ/script/config/$3 ./organ/log/$2/bits_32_relay.txt ./organ/log/$2/bits_226_relay.txt 2> >(tee -a output.log >&2)"
 ssh -i ~/organ.pem ubuntu@$1 $script
-mkdir -p ../../log/$2/$3/
-scp -i ~/organ.pem ubuntu@$1:./output.log ../../log/$2/$3/relay.log
+mkdir -p ../../log/$3/
+scp -i ~/organ.pem ubuntu@$1:./output.log ../../log/$3/relay.log
