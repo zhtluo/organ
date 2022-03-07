fefd7d7b5f4b9605eaffa0cfb86446d2066acbb7
zhtluo <zhtluo@gmail.com>
2022-03-06 20:10:12 -0500

    Updated script

---
+++ script/run_one.sh
@@ -0,0 +1,20 @@
+while IFS= read -r line; do
+  IPS+=($line)
+done < $1
+
+for ip in "${IPS[@]}"
+do
+    ssh -i ~/organ.pem ubuntu@$ip "killall organ" &
+done
+
+wait
+
+cd ./script/config/
+for d in *; do
+  c=$d/$2
+  bash ../run_server.sh ${IPS[0]} $d $c &
+  for ((i = 1; i <= $d; i++)); do
+    bash ../run_client.sh ${IPS[$i]} $d $c $(expr $i - 1) &
+  done
+  wait
+done
