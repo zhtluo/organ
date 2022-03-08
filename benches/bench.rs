67dcd2d3ba6d483296e24970dd29370350a4e47c
zhtluo <zhtluo@gmail.com>
2022-03-08 11:16:37 -0500

    bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -7,8 +7,8 @@ use organ::*;
 use rug::ops::Pow;
 use rug::Integer;
 
-pub fn criterion_benchmark_compute_prf_bulk(cr: &mut Criterion) {
-    let c = config::Config {
+fn get_config(slot: usize) -> config::Config {
+    config::Config {
         server_addr: std::net::SocketAddr::new(
             std::net::IpAddr::V4(std::net::Ipv4Addr::new(0, 0, 0, 0)),
             0,
@@ -49,30 +49,60 @@ pub fn criterion_benchmark_compute_prf_bulk(cr: &mut Criterion) {
         do_unzip: false,
         do_delay: false,
         do_ping: false,
-        slot_per_round: 3,
+        slot_per_round: slot,
         round: 0,
-    };
-    let mut group = cr.benchmark_group("compute_prf_bulk");
+    }
+}
+
+fn timing_base(c: &config::Config, size: usize) {
+    let mut rand = rug::rand::RandState::new();
+    crate::timing::compute(&crate::timing::CompParameters {
+        a: std::iter::repeat_with(|| Integer::from(c.base_params.p.random_below_ref(&mut rand)))
+            .take((c.slot_per_round * size).next_power_of_two())
+            .collect(),
+        b: std::iter::repeat_with(|| Integer::from(c.base_params.p.random_below_ref(&mut rand)))
+            .take((c.slot_per_round * size).next_power_of_two())
+            .collect(),
+        p: c.bulk_params.p.clone(),
+        w: c.bulk_params.ring_v.order.clone(),
+        order: c.bulk_params.q.clone(),
+    });
+}
+
+fn timing_bulk(c: &config::Config, size: usize) {
+    let mut rand = rug::rand::RandState::new();
+    crate::timing::compute(&crate::timing::CompParameters {
+        a: std::iter::repeat_with(|| Integer::from(c.base_params.p.random_below_ref(&mut rand)))
+            .take((c.slot_per_round * size).next_power_of_two())
+            .collect(),
+        b: std::iter::repeat_with(|| Integer::from(c.base_params.p.random_below_ref(&mut rand)))
+            .take((c.slot_per_round * size).next_power_of_two())
+            .collect(),
+        p: c.bulk_params.p.clone(),
+        w: c.bulk_params.ring_v.order.clone(),
+        order: c.bulk_params.q.clone(),
+    });
+}
+
+pub fn criterion_benchmark_compute_prf_bulk(cr: &mut Criterion) {
+    let c = get_config(3);
+    let mut group = cr.benchmark_group("compute_prf_bulk_58");
     for size in [50, 100, 150, 200].iter() {
         group.throughput(Throughput::Bytes(*size as u64));
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
             b.iter(|| {
-                let mut rand = rug::rand::RandState::new();
-                crate::timing::compute(&crate::timing::CompParameters {
-                    a: std::iter::repeat_with(|| {
-                        Integer::from(c.base_params.p.random_below_ref(&mut rand))
-                    })
-                    .take((c.slot_per_round * size).next_power_of_two())
-                    .collect(),
-                    b: std::iter::repeat_with(|| {
-                        Integer::from(c.base_params.p.random_below_ref(&mut rand))
-                    })
-                    .take((c.slot_per_round * size).next_power_of_two())
-                    .collect(),
-                    p: c.bulk_params.p.clone(),
-                    w: c.bulk_params.ring_v.order.clone(),
-                    order: c.bulk_params.q.clone(),
-                });
+                timing_bulk(&c, size);
+            });
+        });
+    }
+    group.finish();
+    let c = get_config(37);
+    let mut group = cr.benchmark_group("compute_prf_bulk_1024");
+    for size in [50, 100, 150, 200].iter() {
+        group.throughput(Throughput::Bytes(*size as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            b.iter(|| {
+                timing_bulk(&c, size);
             });
         });
     }
@@ -80,76 +110,33 @@ pub fn criterion_benchmark_compute_prf_bulk(cr: &mut Criterion) {
 }
 
 pub fn criterion_benchmark_compute_prf_base(cr: &mut Criterion) {
-    let c = config::Config {
-        server_addr: std::net::SocketAddr::new(
-            std::net::IpAddr::V4(std::net::Ipv4Addr::new(0, 0, 0, 0)),
-            0,
-        ),
-        client_size: 200,
-        base_params: ProtocolParams {
-            p: Integer::from(2).pow(64) - 59,
-            q: Integer::from(2).pow(84) - 35,
-            ring_v: NttField {
-                order: (Integer::from(57) * (Integer::from(2).pow(96))) + 1,
-                root: Integer::from_str_radix("2418184924512328812370262861594", 10).unwrap(),
-                scale: Integer::from(2).pow(96),
-            },
-            vector_len: 2048,
-            bits: 32,
-            group_nid: Nid::SECP256K1.as_raw(),
-            group: Some(EcGroup::from_curve_name(Nid::SECP256K1).unwrap()),
-        },
-        bulk_params: ProtocolParams {
-            p: Integer::from(2).pow(226) - 5,
-            // order of secp256k1
-            q: Integer::from_str_radix(
-                "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141",
-                16,
-            )
-            .unwrap(),
-            ring_v: NttField {
-                order: (Integer::from(7) * (Integer::from(2).pow(290))) + 1,
-                root: Integer::from_str_radix("2187", 10).unwrap(),
-                scale: Integer::from(2).pow(290),
-            },
-            vector_len: 8192,
-            bits: 226,
-            group_nid: Nid::SECT571K1.as_raw(),
-            group: Some(EcGroup::from_curve_name(Nid::SECT571K1).unwrap()),
-        },
-        do_blame: false,
-        do_unzip: false,
-        do_delay: false,
-        do_ping: false,
-        slot_per_round: 3,
-        round: 0,
-    };
-    let mut group = cr.benchmark_group("compute_prf_base");
+    let c = get_config(3);
+    let mut group = cr.benchmark_group("compute_prf_base_58");
     for size in [50, 100, 150, 200].iter() {
         group.throughput(Throughput::Bytes(*size as u64));
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
             b.iter(|| {
-                let mut rand = rug::rand::RandState::new();
-                crate::timing::compute(&crate::timing::CompParameters {
-                    a: std::iter::repeat_with(|| {
-                        Integer::from(c.base_params.p.random_below_ref(&mut rand))
-                    })
-                    .take((c.slot_per_round * size).next_power_of_two())
-                    .collect(),
-                    b: std::iter::repeat_with(|| {
-                        Integer::from(c.base_params.p.random_below_ref(&mut rand))
-                    })
-                    .take((c.slot_per_round * size).next_power_of_two())
-                    .collect(),
-                    p: c.bulk_params.p.clone(),
-                    w: c.bulk_params.ring_v.order.clone(),
-                    order: c.bulk_params.q.clone(),
-                });
+                timing_base(&c, size);
+            })
+        });
+    }
+    group.finish();
+    let c = get_config(37);
+    let mut group = cr.benchmark_group("compute_prf_base_1024");
+    for size in [50, 100, 150, 200].iter() {
+        group.throughput(Throughput::Bytes(*size as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            b.iter(|| {
+                timing_base(&c, size);
             })
         });
     }
     group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark_compute_prf_bulk, criterion_benchmark_compute_prf_base);
+criterion_group!(
+    benches,
+    criterion_benchmark_compute_prf_bulk,
+    criterion_benchmark_compute_prf_base
+);
 criterion_main!(benches);
